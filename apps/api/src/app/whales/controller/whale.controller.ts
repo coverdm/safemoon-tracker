a2fd205003265a598b4b170be5248f1066785c0f
Dawid Matuszak <dawid_matuszak@devself.pl>
2021-09-05 19:51:41 +0200

    Fix change whales text

--- apps/api/src/app/whales/controller/whale.controller.ts
+++ apps/api/src/app/whales/controller/whale.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Logger, Param } from '@nestjs/common';
 import { WhaleService } from '../services/whale.service';
 import { Whale } from '../schemas/whale.schema';
 import { WhaleDto } from '../dtos/whale-list.dto';
 
 @Controller('whales')
 export class WhaleController {
+  private readonly logger = new Logger(WhaleController.name);
 
   constructor(private readonly whaleService: WhaleService) {}
 
@@ -17,7 +18,18 @@ export class WhaleController {
   async getWhales(): Promise<WhaleDto[]> {
     return this.whaleService.getWhales()
       .then(whales => {
-        return whales.map(whale => new WhaleDto(whale))
+
+        if (isNaN((+whales[0].balance_history))) {
+          whales[0].balance_current.toString();
+          this.logger.log(`isNan: ${whales[0].balance_current.toString()}`);
+        } else {
+          const current: number = +whales[0].balance_current
+          const history: number = +whales[0].balance_history;
+          this.logger.log(current);
+          this.logger.log(history);
+          this.logger.log((current + history).toString());
+        }
+        return whales.map(whale => new WhaleDto(whale));
       })
   }
 }
