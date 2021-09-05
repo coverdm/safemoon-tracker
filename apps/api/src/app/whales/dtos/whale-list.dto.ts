18b9b7f40d7d7de7f87454a580380c7f5cd881da
Dawid Matuszak <dawid_matuszak@devself.pl>
2021-09-05 19:47:21 +0200

    Fix change whales

--- apps/api/src/app/whales/dtos/whale-list.dto.ts
+++ apps/api/src/app/whales/dtos/whale-list.dto.ts
@@ -7,6 +7,12 @@ export class WhaleDto {
   constructor(whale: Whale) {
     this.address = whale.address;
     this.balance = whale.balance_current;
-    this.change = ((+whale.balance_current) - (+whale.balance_history || 0)).toString();
+    if (isNaN((+whale.balance_history))) {
+      this.change = whale.balance_current.toString();
+    } else {
+      const current: number = +whale.balance_current
+      const history: number = +whale.balance_history;
+      this.change = (current - history).toString();
+    }
   }
 }
